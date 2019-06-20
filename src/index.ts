02452145e32a50a5d2918e9cf8008e35acbf6db9
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-06-20 15:07:12 +0200

    ripper

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 export const Greeter = (name: string) => `Hello ${name}`;
+
+export class Ripper {
+    private name: string;
+    constructor( ) {
+        this.name = "default name";
+    }
+    public getName(): string {
+        return this.name;
+    }
+    public setName(name: string) {
+        this.name = name;
+    }
+}
+
