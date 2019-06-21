a3c2cf5f41a3d24025030e19a642add06b04bab7
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-06-21 15:21:37 +0200

    1.0.6

--- src/__tests__/FioReader.test.ts
+++ src/__tests__/FioReader.test.ts
@@ -4,8 +4,10 @@ const fetchMock = fetch as FetchMock;
 import { FioReader } from '../index';
 test('My FioReader', async () => {
   fetchMock.resetMocks();
-  fetchMock.mockResponseOnce('{"accountStatement":{"info":{"accountId":"23231","bankId":"2010","currency":"CZK","iban":"CZ81201000000023231","bic":"FIOBCZPPXXX","openingBalance":1.23,"closingBalance":1.42,"dateStart":"2019-06-21+0200","dateEnd":"2019-06-21+0200","yearList":null,"idList":null,"idFrom":null,"idTo":null,"idLastDownload":213131313},"transactionList":{"transaction":[]}}}');
-  const fr = new FioReader("test_token");
+  fetchMock.mockResponseOnce(
+    '{"accountStatement":{"info":{"accountId":"23231","bankId":"2010","currency":"CZK","iban":"CZ81201000000023231","bic":"FIOBCZPPXXX","openingBalance":1.23,"closingBalance":1.42,"dateStart":"2019-06-21+0200","dateEnd":"2019-06-21+0200","yearList":null,"idList":null,"idFrom":null,"idTo":null,"idLastDownload":213131313},"transactionList":{"transaction":[]}}}',
+  );
+  const fr = new FioReader('test_token');
   const frr = await fr.getRecord();
   // expect(frr).toMatchObject({ title: 'title test' });
   expect(frr.accountStatement.info.accountId).toBe('23231');
--- src/index.ts
+++ src/index.ts
@@ -23,29 +23,28 @@ export interface IFioRecord {
 }
 
 export interface IFioAccountStatement {
-    info: IFioInfo;
+  info: IFioInfo;
 }
 
 export interface IFioInfo {
-    accountId: string;
-    bankId: string;
-    currency: string;
+  accountId: string;
+  bankId: string;
+  currency: string;
 }
 
 // tslint:disable-next-line:max-classes-per-file
 export class FioReader {
   private apiUrl = 'https://www.fio.cz/ib_api/rest/';
-  private apiToken: string;  
+  private apiToken: string;
   constructor(token: string) {
     this.apiToken = token;
   }
 
-
   public async getRecord(): Promise<IFioRecord> {
-   // const r = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-   const r = await fetch(this.apiUrl + 'last/' + this.apiToken + '/transactions.json');
-  // const jt = await r.text();
-   const js = await r.json();
+    // const r = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const r = await fetch(this.apiUrl + 'last/' + this.apiToken + '/transactions.json');
+    // const jt = await r.text();
+    const js = await r.json();
     return js;
   }
 }
