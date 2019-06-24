873dbadbfde7273a796d44158160cea86a259904
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-06-24 14:33:34 +0200

    1.0.8

--- src/fio.ts
+++ src/fio.ts
@@ -12,59 +12,57 @@ export interface IFioAccountStatement {
 }
 
 export interface IFioTrList {
-    transaction: IFioTransaction[];
+  transaction: IFioTransaction[];
 }
 
 export interface IFioTransaction {
-    id: number
-    date: string;
-    amount: number;
-    currency: string;
-    type: string;
-    fAccountId: string | null;
-    fBankId: string | null;
-    fAccountName: string | null;
-    fBankName: string | null;
+  id: number;
+  date: string;
+  amount: number;
+  currency: string;
+  type: string;
+  fAccountId: string | null;
+  fBankId: string | null;
+  fAccountName: string | null;
+  fBankName: string | null;
 
-    ks: string | null;
-    vs: string | null;
-    ss: string | null;
-    userRef: string | null;
-    userMsg: string | null;
-    comment: string | null;
+  ks: string | null;
+  vs: string | null;
+  ss: string | null;
+  userRef: string | null;
+  userMsg: string | null;
+  comment: string | null;
 }
 
 export interface IFioRawTransaction {
-    column22: IFioRawTrRecordN;  // ID pohybu
-    column0: IFioRawTrRecordS; // Datum 
-    column1: IFioRawTrRecordN;  // Objem
-    column14: IFioRawTrRecordS; // Mena
-    column8: IFioRawTrRecordS; // Typ
+  column22: IFioRawTrRecordN; // ID pohybu
+  column0: IFioRawTrRecordS; // Datum
+  column1: IFioRawTrRecordN; // Objem
+  column14: IFioRawTrRecordS; // Mena
+  column8: IFioRawTrRecordS; // Typ
 
-    column2: IFioRawTrRecordS; // protiucet
-    column3: IFioRawTrRecordS; // protiucet banka
-    column10: IFioRawTrRecordS; // protiucet nazev
-    column12: IFioRawTrRecordS; // protiucet banka nazev
+  column2: IFioRawTrRecordS; // protiucet
+  column3: IFioRawTrRecordS; // protiucet banka
+  column10: IFioRawTrRecordS; // protiucet nazev
+  column12: IFioRawTrRecordS; // protiucet banka nazev
 
-    column4: IFioRawTrRecordS; // KS
-    column5: IFioRawTrRecordS; // VS
-    column6: IFioRawTrRecordS; // SS
-    column7: IFioRawTrRecordS; // Uživatelská identifikace
-    column16: IFioRawTrRecordS; // Zpráva pro příjemce
-    column25: IFioRawTrRecordS; // Komentář
-    
+  column4: IFioRawTrRecordS; // KS
+  column5: IFioRawTrRecordS; // VS
+  column6: IFioRawTrRecordS; // SS
+  column7: IFioRawTrRecordS; // Uživatelská identifikace
+  column16: IFioRawTrRecordS; // Zpráva pro příjemce
+  column25: IFioRawTrRecordS; // Komentář
 }
 export interface IFioRawTrRecordS {
-    value: string;
-    name: string;
-    id: number;
+  value: string;
+  name: string;
+  id: number;
 }
 export interface IFioRawTrRecordN {
-    value: number;
-    name: string;
-    id: number;
+  value: number;
+  name: string;
+  id: number;
 }
-
 
 export interface IFioInfo {
   accountId: string;
@@ -77,8 +75,6 @@ export interface IFioInfo {
   idLastDownload: number | null;
 }
 
-
-
 // tslint:disable-next-line:max-classes-per-file
 export class FioReader {
   private apiUrl = 'https://www.fio.cz/ib_api/rest/';
@@ -86,7 +82,6 @@ export class FioReader {
   constructor(token: string) {
     this.apiToken = token;
   }
-
 
   public async getLast(): Promise<IFioRecord> {
     const r = await fetch(this.apiUrl + 'last/' + this.apiToken + '/transactions.json');
@@ -96,13 +91,18 @@ export class FioReader {
 
   public async getPeriods(fromDate: Date, toDate: Date): Promise<IFioRecord> {
     const r = await fetch(
-        this.apiUrl + 'periods/' + 
-        this.apiToken + '/' +
-        this.date2fioParam(fromDate) + '/' + this.date2fioParam(toDate) + '/transactions.json');
+      this.apiUrl +
+        'periods/' +
+        this.apiToken +
+        '/' +
+        this.date2fioParam(fromDate) +
+        '/' +
+        this.date2fioParam(toDate) +
+        '/transactions.json',
+    );
     const js = await r.json();
     return this.raw2fr(js);
   }
-
 
   private async _getRaw(): Promise<string> {
     // const r = await fetch('https://jsonplaceholder.typicode.com/todos/1');
@@ -115,62 +115,65 @@ export class FioReader {
   }
 
   private date2fioParam(d: Date): string {
-    const yp = d.getUTCFullYear().toString(10).padStart(2,'0');
-    const mp = (d.getUTCMonth()+1).toString(10).padStart(2,'0');
-    const dp = d.getUTCDate().toString(10).padStart(2,'0');
+    const yp = d
+      .getUTCFullYear()
+      .toString(10)
+      .padStart(2, '0');
+    const mp = (d.getUTCMonth() + 1).toString(10).padStart(2, '0');
+    const dp = d
+      .getUTCDate()
+      .toString(10)
+      .padStart(2, '0');
     return yp + '-' + mp + '-' + dp;
-}
-
+  }
 
   private raw2tr(rt: IFioRawTransaction): IFioTransaction {
-      return {
-          // mandatory fields:
-          id: rt.column22.value,
-          // tslint:disable-next-line:object-literal-sort-keys
-          date: rt.column0.value,
-          amount: rt.column1.value,
-          currency: rt.column14.value,
-          type: rt.column8.value,
+    return {
+      // mandatory fields:
+      id: rt.column22.value,
+      // tslint:disable-next-line:object-literal-sort-keys
+      date: rt.column0.value,
+      amount: rt.column1.value,
+      currency: rt.column14.value,
+      type: rt.column8.value,
 
-          // optional fields:
-          fAccountId: rt.column2 ? rt.column2.value : null,
-          fBankId: rt.column3 ? rt.column3.value : null,
-          fAccountName: rt.column10 ? rt.column10.value : null,
-          fBankName: rt.column12 ? rt.column12.value : null,
+      // optional fields:
+      fAccountId: rt.column2 ? rt.column2.value : null,
+      fBankId: rt.column3 ? rt.column3.value : null,
+      fAccountName: rt.column10 ? rt.column10.value : null,
+      fBankName: rt.column12 ? rt.column12.value : null,
 
-
-          ks: rt.column4 ? rt.column4.value : null,
-          vs: rt.column5 ? rt.column5.value : null,
-          ss: rt.column6 ? rt.column6.value : null,
-          userRef: rt.column7 ? rt.column7.value : null,
-          userMsg: rt.column16 ? rt.column16.value : null,
-          comment: rt.column25 ? rt.column25.value : null,
-      }
+      ks: rt.column4 ? rt.column4.value : null,
+      vs: rt.column5 ? rt.column5.value : null,
+      ss: rt.column6 ? rt.column6.value : null,
+      userRef: rt.column7 ? rt.column7.value : null,
+      userMsg: rt.column16 ? rt.column16.value : null,
+      comment: rt.column25 ? rt.column25.value : null,
+    };
   }
   private raw2fr(js: any): IFioRecord {
-    return { 
-        userId: js.userId,
-        // tslint:disable-next-line:object-literal-sort-keys
-        id: js.id,
-        title: js.title,
-        completed: js.completed,
-        accountStatement: {
-            info: {
-                accountId: js.accountStatement.info.accountId,
-                bankId: js.accountStatement.info.bankId,
-                currency: js.accountStatement.info.currency,
-                iban: js.accountStatement.info.iban,
-                // tslint:disable-next-line:object-literal-sort-keys
-                bic: js.accountStatement.info.bic,
-                idFrom: js.accountStatement.info.idFrom,
-                idTo: js.accountStatement.info.idTo,
-                idLastDownload: js.accountStatement.info.idLastDownload
-            },
-            transactionList: {
-                transaction: js.accountStatement.transactionList.transaction.map( (rt: IFioRawTransaction)=>this.raw2tr(rt))
-            }
-        }
+    return {
+      userId: js.userId,
+      // tslint:disable-next-line:object-literal-sort-keys
+      id: js.id,
+      title: js.title,
+      completed: js.completed,
+      accountStatement: {
+        info: {
+          accountId: js.accountStatement.info.accountId,
+          bankId: js.accountStatement.info.bankId,
+          currency: js.accountStatement.info.currency,
+          iban: js.accountStatement.info.iban,
+          // tslint:disable-next-line:object-literal-sort-keys
+          bic: js.accountStatement.info.bic,
+          idFrom: js.accountStatement.info.idFrom,
+          idTo: js.accountStatement.info.idTo,
+          idLastDownload: js.accountStatement.info.idLastDownload,
+        },
+        transactionList: {
+          transaction: js.accountStatement.transactionList.transaction.map((rt: IFioRawTransaction) => this.raw2tr(rt)),
+        },
+      },
     };
-  } 
-
+  }
 }
