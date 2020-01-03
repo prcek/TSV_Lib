e98d29348da81abc24a89f9d2a1f0114b6d410b9
Tomas Hluchan <tomas.hluchan@gmail.com>
2020-01-03 23:16:31 +0100

    report cash

--- src/__tests__/ReportPayments.test.ts
+++ src/__tests__/ReportPayments.test.ts
@@ -8,7 +8,6 @@ import {
   ICoursePayment,
   IStudentInfo,
 } from '../course_payments';
-import { FioDataStore, FioTransactionType, IFioBankTransaction } from '../fio_ds';
 import { createMongooseConnection, mongod } from '../jestutils';
 import { ICategoryInfo, ReportPayments } from '../report_payments';
 
@@ -73,17 +72,17 @@ const lookupcourse = jest.fn(
 );
 
 const lookupcategory = jest.fn(
-  (catKey: string): Promise<ICategoryInfo | null> => {
+  (catKey: string, seasonKey:string): Promise<ICategoryInfo | null> => {
     let ci: ICategoryInfo | null = null;
     switch (catKey) {
       case 'f1':
-        ci = { key: catKey, firmKey: 'firm1', courseKeys: ['c1', 'c2'], name: 'cat1' };
+        ci = { key: catKey, seasonKey, firmKey: 'firm1', courseKeys: ['c1', 'c2'], name: 'cat1' };
         break;
       case 'f2':
-        ci = { key: catKey, firmKey: 'firm1', courseKeys: ['c1'], name: 'cat2' };
+        ci = { key: catKey, seasonKey, firmKey: 'firm1', courseKeys: ['c1'], name: 'cat2' };
         break;
       case 'f3':
-        ci = { key: catKey, firmKey: 'firm2', courseKeys: [], name: 'cat3' };
+        ci = { key: catKey, seasonKey, firmKey: 'firm2', courseKeys: [], name: 'cat3' };
         break;
     }
     return Promise.resolve(ci);
@@ -149,7 +148,85 @@ test('CoursePayments - get multi payments', async () => {
     coursePaymentsStore: cps,
     endDate: new Date('1996-01-01T00:00'),
   });
-  expect(await rps.createReport()).toMatchObject({ done: true });
+
+
+  const cat1 = await lookupcategory('f1','s1');
+  expect(cat1).toMatchObject({key:'f1'});
+  expect(cat1?.courseKeys).toStrictEqual(['c1','c2'])
+  expect(cat1?.firmKey).toBe('firm1');
+
+
+  expect(await rps.addCategory('f1','s1')).toBe(true);
+  const rep1 = await rps.createReportUpToDate(null);
+  expect(rep1).toHaveLength(1);
+  expect(rep1[0].courses).toHaveLength(2);
+
+  expect(rep1[0].amount).toBe(6600);
+  expect(rep1[0].amountByStatus.e).toBe(0);
+  expect(rep1[0].amountByStatus.s).toBe(0);
+  expect(rep1[0].amountByStatus.n).toBe(4900);
+  expect(rep1[0].amountByStatus.k).toBe(1700);
+
+  expect(rep1[0].cat.name).toBe('cat1');
+  expect(rep1[0].cat.firmKey).toBe('firm1');
+
+  expect(await rps.addCategory('f2','s1')).toBe(true);
+
+  const rep2 = await rps.createReportUpToDate(null);
+  expect(rep2).toHaveLength(2);
+  expect(rep2[0].courses).toHaveLength(2);
+  expect(rep2[0].amount).toBe(6600);
+  expect(rep2[0].amountByStatus.e).toBe(0);
+  expect(rep2[0].amountByStatus.s).toBe(0);
+  expect(rep2[0].amountByStatus.n).toBe(4900);
+  expect(rep2[0].amountByStatus.k).toBe(1700);
+  expect(rep2[0].date).toBe(null);
+ 
+ 
+  expect(rep2[1].courses).toHaveLength(1);
+  expect(rep2[1].amount).toBe(4900);
+  expect(rep2[1].amountByStatus.e).toBe(0);
+  expect(rep2[1].amountByStatus.s).toBe(0);
+  expect(rep2[1].amountByStatus.n).toBe(4900);
+  expect(rep2[1].amountByStatus.k).toBe(0);
+  expect(rep2[1].date).toBe(null);
+ 
+  expect(await rps.clearCategories()).toBe(true);
+  const repe = await rps.createReportUpToDate(null);
+  expect(repe).toHaveLength(0);
+
+
+  expect(await rps.addCategory('f2','s1')).toBe(true);
+  const rep3 = await rps.createReportUpToDate(new Date('1996-01-01T00:00:00.000Z'));
+  expect(rep3).toHaveLength(1);
+  expect(rep3[0].amount).toBe(3200);
+  expect(rep3[0].amountByStatus.e).toBe(0);
+  expect(rep3[0].amountByStatus.s).toBe(0);
+  expect(rep3[0].amountByStatus.n).toBe(3200);
+  expect(rep3[0].amountByStatus.k).toBe(0);
+
+
+  expect(rep3[0].date).toEqual(new Date('1996-01-01T00:00:00.000Z'));
+
+  const rep4 = await rps.createReportUpToDate(new Date('1996-01-01T04:00:00.000Z'));
+  expect(rep4).toHaveLength(1);
+  expect(rep4[0].amount).toBe(4900);
+  expect(rep4[0].amountByStatus.e).toBe(0);
+  expect(rep4[0].amountByStatus.s).toBe(0);
+  expect(rep4[0].amountByStatus.n).toBe(4900);
+  expect(rep4[0].amountByStatus.k).toBe(0);
+
+
+  expect(await rps.clearCategories()).toBe(true);
+  expect(await rps.addCourses(['c1','c2'])).toBe(true);
+  const rep5= await rps.createReportUpToDate(new Date('1996-01-01T00:00:00.000Z'));
+  expect(rep5).toHaveLength(1);
+  expect(rep5[0].amount).toBe(3200);
+  expect(rep5[0].amountByStatus.e).toBe(0);
+  expect(rep5[0].amountByStatus.s).toBe(0);
+  expect(rep5[0].amountByStatus.n).toBe(3200);
+  expect(rep5[0].amountByStatus.k).toBe(0);
+
 
   mc.close();
 });
--- src/report_payments.ts
+++ src/report_payments.ts
@@ -1,15 +1,16 @@
 // import { MongoError } from 'mongodb';
 // import * as mongoose from 'mongoose';
 import * as R from 'ramda';
-import { CoursePaymentsStore } from './course_payments';
+import { CoursePaymentsStore, EStudentStatusType, ICoursePaymentsReport } from './course_payments';
 
 export interface ICategoryInfo {
   key: string;
+  seasonKey: string;
   firmKey: string;
   courseKeys: string[];
   name: string;
 }
-type TLookupCategoryInfoType = (catKey: string) => Promise<ICategoryInfo | null>;
+type TLookupCategoryInfoType = (catKey: string, seasonKey:string) => Promise<ICategoryInfo | null>;
 
 interface IReportPaymentsOptions {
   coursePaymentsStore: CoursePaymentsStore;
@@ -17,8 +18,13 @@ interface IReportPaymentsOptions {
   endDate: Date;
 }
 
+
 interface IReportPaymentsResult {
-  done: boolean;
+    cat: ICategoryInfo;
+    date: Date | null;
+    courses: Array<ICoursePaymentsReport |null>;
+    amount: number;
+    amountByStatus: Record<EStudentStatusType, number>;
 }
 export class ReportPayments {
   private coursePaymentsStore: CoursePaymentsStore;
@@ -36,14 +42,56 @@ export class ReportPayments {
     this.cats = [];
     return true;
   }
-  public async addCategory(catKey: string): Promise<boolean> {
-    const cat = await this.lookupCategoryInfo(catKey);
+  public async addCategory(catKey: string, seasonKey: string): Promise<boolean> {
+    const cat = await this.lookupCategoryInfo(catKey, seasonKey);
     if (cat) {
+      this.cats.push(cat);
       return true;
     }
     return false;
   }
-  public async createReport(): Promise<IReportPaymentsResult | null> {
-    return { done: true };
+
+  public async addCourses(courseKeys: string[]): Promise<boolean> {
+    const cat: ICategoryInfo = {
+        key: "",
+        seasonKey: "",
+        firmKey: "",
+        courseKeys,
+        name: "",
+    }
+    if (cat) {
+      this.cats.push(cat);
+      return true;
+    }
+    return false;
+  }
+
+  public async createReportUpToDate(toDate: Date | null): Promise<IReportPaymentsResult[]> {
+    const rs: IReportPaymentsResult[] = []
+    for(const cat of this.cats) {
+        // console.log('createReport - cat', cat.name);
+        const catR: IReportPaymentsResult = {
+            cat,
+            courses:[],
+            date: toDate,
+            amount: 0,
+            amountByStatus:  {s:0, n:0, k:0, e:0}
+        }
+        for(const courseKey of cat.courseKeys) {
+            // console.log('createReport - courseKey');
+            const r = await ( toDate ?  this.coursePaymentsStore.getCoursePaymentsUpToDate(courseKey, toDate): this.coursePaymentsStore.getCoursePayments(courseKey));
+            catR.courses.push(r);
+            if (r) {
+                catR.amount += r.amount;
+                catR.amountByStatus.e += r.amountByStatus.e;
+                catR.amountByStatus.s += r.amountByStatus.s;
+                catR.amountByStatus.n += r.amountByStatus.n;
+                catR.amountByStatus.k += r.amountByStatus.k;
+            }
+            
+        }
+        rs.push(catR);
+    }  
+    return rs;
   }
 }
