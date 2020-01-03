c4e89071659e465fdde35905e9735bed96b449be
Tomas Hluchan <tomas.hluchan@gmail.com>
2020-01-03 16:59:05 +0100

    payments report wip

--- src/__tests__/CoursePayments.test.ts
+++ src/__tests__/CoursePayments.test.ts
@@ -1,6 +1,6 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import * as mongoose from 'mongoose';
-import { CoursePaymentsStore, ECoursePaymentType, ICoursePayment, IStudentInfo } from '../course_payments';
+import { CoursePaymentsStore, ECoursePaymentType, EStudentStatusType, ICourseInfo, ICoursePayment, IStudentInfo } from '../course_payments';
 import { FioDataStore, FioTransactionType, IFioBankTransaction } from '../fio_ds';
 
 import { createMongooseConnection, mongod } from '../jestutils';
@@ -18,16 +18,16 @@ const lookup = jest.fn(
     let si: IStudentInfo | null = null;
     switch (vs) {
       case '1':
-        si = { studentKey: 'st1', firmKey: 'f1', courseCost: 1000, paid: 0 };
+        si = { studentKey: 'st1', firmKey: 'f1', courseCost: 1000, paid: 0, status: EStudentStatusType.NEW };
         break;
       case '2':
-        si = { studentKey: 'st2', firmKey: 'f1', courseCost: 1200, paid: 0 };
+        si = { studentKey: 'st2', firmKey: 'f1', courseCost: 1200, paid: 0, status: EStudentStatusType.NEW};
         break;
       case '3':
-        si = { studentKey: 'st3', firmKey: 'f2', courseCost: 1200, paid: 800 };
+        si = { studentKey: 'st3', firmKey: 'f2', courseCost: 1200, paid: 800, status: EStudentStatusType.NEW };
         break;
       case '4':
-        si = { studentKey: 'st4', firmKey: 'f2', courseCost: 1200, paid: 0 };
+        si = { studentKey: 'st4', firmKey: 'f2', courseCost: 1200, paid: 0, status: EStudentStatusType.NEW };
         break;
     }
     return Promise.resolve(si);
@@ -40,6 +40,22 @@ const update = jest.fn(
   },
 );
 
+const lookupcourse = jest.fn(
+  (key: string): Promise<ICourseInfo | null> => {
+    let ci: ICourseInfo | null = null;
+    switch (key) {
+      case 'c1':
+        ci = {firmKey:'f1', students: [{ studentKey: 'st1', firmKey: 'f1', courseCost: 1000, paid: 0, status: EStudentStatusType.NEW }, { studentKey: 'st2', firmKey: 'f1', courseCost: 1200, paid: 0, status: EStudentStatusType.NEW }, { studentKey: 'st4', firmKey: 'f1', courseCost: 1000, paid: 0, status: EStudentStatusType.KICK }]}
+        break;
+      case 'c2':
+        ci = {firmKey:'f1', students: [{ studentKey: 'st3', firmKey: 'f1', courseCost: 1200, paid: 0 , status: EStudentStatusType.KICK}]}
+        break;
+    }
+    return Promise.resolve(ci);
+  },
+);
+
+
 test('CoursePayments - lookup Exact', async () => {
   // lookup.mockResolvedValueOnce({
   //   key: "1",
@@ -50,6 +66,7 @@ test('CoursePayments - lookup Exact', async () => {
   const mc = await createMongooseConnection(muri);
   const cps = new CoursePaymentsStore(mc, {
     lookupStudentInfo: lookup,
+    lookupCourseInfo: lookupcourse,
     updateStudentPaymentInfo: update,
     firmAccounts: { f1: ['ac1'], f2: ['ac2', 'ac3'] },
   });
@@ -72,6 +89,7 @@ test('CoursePayments - insert payments', async () => {
   const mc = await createMongooseConnection(muri);
   const cps = new CoursePaymentsStore(mc, {
     lookupStudentInfo: lookup,
+    lookupCourseInfo: lookupcourse,
     updateStudentPaymentInfo: update,
     firmAccounts: { f1: ['ac1'], f2: ['ac2', 'ac3'] },
   });
@@ -100,6 +118,7 @@ test('CoursePayments tryAuto', async () => {
   const mc = await createMongooseConnection(muri);
   const cps = new CoursePaymentsStore(mc, {
     lookupStudentInfo: lookup,
+    lookupCourseInfo: lookupcourse,
     updateStudentPaymentInfo: update,
     firmAccounts: { f1: ['ac1'], f2: ['ac2', 'ac3'] },
   });
@@ -134,3 +153,113 @@ test('CoursePayments tryAuto', async () => {
   expect(ps[0]).toMatchObject({ fioTrRef: nnt._id.toString() });
   mc.close();
 });
+
+
+test.only('CoursePayments - get multi payments', async () => {
+  update.mockClear();
+  const muri = await mongod.getConnectionString();
+  const mc = await createMongooseConnection(muri);
+  const cps = new CoursePaymentsStore(mc, {
+    lookupStudentInfo: lookup,
+    lookupCourseInfo: lookupcourse,
+    updateStudentPaymentInfo: update,
+    firmAccounts: { f1: ['ac1'], f2: ['ac2', 'ac3'] },
+  });
+
+  const np = {
+    type: ECoursePaymentType.AUTO,
+    date: new Date('1995-12-17T03:24:00'),
+    amount: 1000,
+    firmKey: 'f1',
+    studentKey: 'st1',
+  } as ICoursePayment;
+
+  expect(await cps.storeNewPayment(np)).toMatchObject({ _id: expect.anything() });
+  expect(update).toHaveBeenCalledTimes(1);
+  expect(update).toHaveBeenCalledWith('st1', 1000);
+  expect(await cps.storeNewPayment(np)).toMatchObject({ _id: expect.anything() });
+  expect(update).toHaveBeenCalledTimes(2);
+  expect(update).toHaveBeenCalledWith('st1', 2000);
+
+  const np2 = {
+    type: ECoursePaymentType.AUTO,
+    date: new Date('1995-12-23T03:24:00'),
+    amount: 1200,
+    firmKey: 'f1',
+    studentKey: 'st2',
+  } as ICoursePayment;
+
+  const np3 = {
+    type: ECoursePaymentType.AUTO,
+    date: new Date('1996-01-01T03:24:00'),
+    amount: 1700,
+    firmKey: 'f1',
+    studentKey: 'st2',
+  } as ICoursePayment;
+
+  const np4 = {
+    type: ECoursePaymentType.AUTO,
+    date: new Date(),
+    amount: 1700,
+    firmKey: 'f1',
+    studentKey: 'st3',
+  } as ICoursePayment;
+
+  expect(await cps.storeNewPayment(np2)).toMatchObject({ _id: expect.anything() });
+  expect(await cps.storeNewPayment(np3)).toMatchObject({ _id: expect.anything() });
+  expect(await cps.storeNewPayment(np4)).toMatchObject({ _id: expect.anything() });
+
+  const ps = await cps.getStudentsPayments(['st1','st2']);
+  expect(ps).toMatchObject({"st1": expect.anything()});
+  expect(ps).toMatchObject({"st2": expect.anything()});
+  expect(ps).not.toMatchObject({"st3": expect.anything()});
+ 
+  // tslint:disable-next-line:no-string-literal
+  expect(ps['st1']).toMatchObject({"amount":2000, payments: expect.any(Array)})
+  // tslint:disable-next-line:no-string-literal
+  expect(ps['st2']).toMatchObject({"amount":2900, payments: expect.any(Array)})
+ 
+  const ps2 = await cps.getStudentsPaymentsUpToDate(['st1','st2'], new Date('1996-01-01T00:00'));
+  expect(ps2).toMatchObject({"st1": expect.anything()});
+  // tslint:disable-next-line:no-string-literal
+  expect(ps2['st1']).toMatchObject({"amount":2000, payments: expect.any(Array)})
+  // tslint:disable-next-line:no-string-literal
+  expect(ps2['st2']).toMatchObject({"amount":1200, payments: expect.any(Array)})
+
+
+
+  const ci1 = await lookupcourse("c1");
+  expect(ci1).not.toBeNull();
+  if (ci1) {
+    expect(ci1).toMatchObject({"firmKey":"f1", "students":expect.any(Array)});
+    // tslint:disable-next-line:no-string-literal
+    expect(ci1['students']).toHaveLength(3);
+  }
+
+  const ps3 = await cps.getCoursePayments("c1");
+  expect(ps3).not.toBeNull();
+  if (ps3) {
+  
+   expect(ps3).toMatchObject({"amount":4900, students: expect.any(Object)});
+   expect(ps3.studentsByStatus).toMatchObject({n:expect.any(Object),s:expect.any(Object),k:expect.any(Object),e:expect.any(Object)});
+   expect(ps3.amountByStatus).toMatchObject({n:4900,s:0,k:0,e:0});
+   expect(ps3.studentsByStatus.n).toMatchObject({st1:expect.any(Object),st2:expect.any(Object)});
+   expect(ps3.studentsByStatus.k).toMatchObject({st4:expect.any(Object)});
+   expect(ps3.countByStatus).toMatchObject({s:0,n:2,e:0,k:1});
+   expect(ps3.count).toBe(3);
+  }
+ 
+
+  const ps4 = await cps.getCoursePaymentsUpToDate("c1", new Date('1996-01-01T00:00'));
+  expect(ps4).not.toBeNull();
+  if (ps4) {
+    expect(ps4).toMatchObject({"amount":3200, students: expect.any(Object)});
+    expect(ps4.amountByStatus).toMatchObject({s:0,n:3200,e:0,k:0});
+    expect(ps4.countByStatus).toMatchObject({s:0,n:2,e:0,k:1});
+    expect(ps4.count).toBe(3);
+  }
+
+
+  mc.close();
+});
+
--- src/course_payments.ts
+++ src/course_payments.ts
@@ -3,11 +3,24 @@ import * as mongoose from 'mongoose';
 import * as R from 'ramda';
 import { FioTransactionProcessingStatus, FioTransactionType, IFioBankTransaction } from './fio_ds';
 
+export enum EStudentStatusType {
+    SPARE = 's',
+    NEW = 'n',
+    ENROLL = 'e',
+    KICK = 'k'
+}
+
 export interface IStudentInfo {
   studentKey: string;
   firmKey: string;
+  status: EStudentStatusType;
   courseCost: number;
   paid: number;
+}
+
+export interface ICourseInfo {
+  students: IStudentInfo[];
+  firmKey: string;
 }
 
 export enum ECoursePaymentType {
@@ -30,14 +43,30 @@ export interface ICoursePayment {
   manualComment: string | null;
 }
 
+export interface IStudentPayments {
+  payments: ICoursePayment[];
+  amount: number;
+} 
+
+export interface ICoursePaymentsReport {
+  students: Record<string,IStudentPayments>;
+  studentsByStatus: Record<EStudentStatusType,Record<string,IStudentPayments>>;
+  amount: number;
+  amountByStatus: Record<EStudentStatusType,number>;
+  count: number;
+  countByStatus: Record<EStudentStatusType,number>;
+} 
+
 export interface ICoursePaymentModel extends mongoose.Document, ICoursePayment {}
 
 type TLookupStudentInfoType = (vs: string) => Promise<IStudentInfo | null>;
+type TLookupCourseInfoType = (key: string) => Promise<ICourseInfo | null>;
 type TFirmAccounts = Record<string, string[]>;
 type TUpdateStudentPaymentInfo = (studentKey: string, paid: number) => Promise<boolean>;
 
 interface ICoursePaymentsStoreOptions {
   lookupStudentInfo: TLookupStudentInfoType;
+  lookupCourseInfo: TLookupCourseInfoType;
   updateStudentPaymentInfo: TUpdateStudentPaymentInfo;
   firmAccounts: TFirmAccounts;
 }
@@ -76,6 +105,7 @@ export class CoursePaymentsStore {
   ).index({ studentKey: 1 });
 
   private lookupStudentInfo: TLookupStudentInfoType;
+  private lookupCourseInfo: TLookupCourseInfoType;
   private updateStudentPaymentInfo: TUpdateStudentPaymentInfo;
   private firmAccounts: TFirmAccounts;
   constructor(mcon: mongoose.Connection, opts: ICoursePaymentsStoreOptions) {
@@ -87,6 +117,7 @@ export class CoursePaymentsStore {
     );
 
     this.lookupStudentInfo = opts.lookupStudentInfo;
+    this.lookupCourseInfo = opts.lookupCourseInfo;
     this.updateStudentPaymentInfo = opts.updateStudentPaymentInfo;
     this.firmAccounts = opts.firmAccounts;
   }
@@ -175,6 +206,93 @@ export class CoursePaymentsStore {
     }
     const paid = payments.reduce((p, cp) => p + cp.amount, 0);
     return paid;
+  }
+
+  public async getStudentsPayments(studentKeys: string[]): Promise<Record<string,IStudentPayments>> {
+    const payments = await this.coursePaymentModel.find({ studentKey: {$in:studentKeys} }).sort({ date: 1 });
+    return this.payments2sp(payments,studentKeys);
+  }
+
+
+  public async getStudentsPaymentsUpToDate(studentKeys: string[], toDate: Date): Promise<Record<string,IStudentPayments>> {
+    const payments = await this.coursePaymentModel.find({ studentKey: {$in:studentKeys}, date: {$lt: toDate} }).sort({ date: 1 });
+    return this.payments2sp(payments,studentKeys);
+  }
+
+  public async getCoursePayments(courseKey: string): Promise<ICoursePaymentsReport | null> {
+    const ci = await this.lookupCourseInfo(courseKey);
+    if (ci) {
+      const studentKeys = R.map(s=>s.studentKey,ci.students);
+      const sps = await this.getStudentsPayments(studentKeys);
+      return this.cpayments2cpr(ci, sps);
+    }
+    return null;
+  }
+
+
+  public async getCoursePaymentsUpToDate(courseKey: string, toDate: Date): Promise<ICoursePaymentsReport|null> {
+    const ci = await this.lookupCourseInfo(courseKey);
+    if (ci) {
+      const studentKeys = R.map(s=>s.studentKey,ci.students);
+      const sps = await this.getStudentsPaymentsUpToDate(studentKeys, toDate);
+      return this.cpayments2cpr(ci, sps);
+    }
+    return null;
+  }
+  
+
+  private cpayments2cpr(ci: ICourseInfo, sps: Record<string,IStudentPayments>):ICoursePaymentsReport {
+    const ssg = R.groupBy(s=>s.status,ci.students);
+    const ssgkeys = R.keys(ssg);
+
+    const sbs: Record<EStudentStatusType,Record<string,IStudentPayments>> = {
+      s: {},
+      n: {},
+      e: {},
+      k: {},
+    }
+
+    const abs: Record<EStudentStatusType,number> = {
+      s:0,
+      n:0,
+      e:0,
+      k:0,
+    }
+
+    const cbs: Record<EStudentStatusType,number> = {
+      s:0,
+      n:0,
+      e:0,
+      k:0,
+    }
+
+    for (const  key of ssgkeys) {
+      const sks = ci.students.filter(s=>s.status === key).map(s=>s.studentKey);
+      const sbsx = R.pick(sks,sps);
+      sbs[key as EStudentStatusType] = sbsx;
+      const am = R.values(sbsx).reduce((a,s)=>a+s.amount,0);
+      abs[key as EStudentStatusType] = am;
+      const cn = R.keys(sbsx).length;
+      cbs[key as EStudentStatusType] = cn;
+    }
+   
+    const amount = R.reduce<IStudentPayments,number>((a,s)=>a+s.amount,0,R.values(sps));
+    const count = R.keys(sps).length;
+    return {amount, count, students:sps, studentsByStatus:sbs, amountByStatus:abs, countByStatus:cbs};
+  }
+
+  private payments2sp(payments:ICoursePayment[], keys:string[]): Record<string,IStudentPayments> {
+    const rr =  R.groupBy<ICoursePayment>(p=>p.studentKey,payments);  
+    
+    const sksm = keys.filter(k=>!(k in rr));
+    for (const mk of sksm) {
+        rr[mk]=[];
+    }
+    
+    return R.map<Record<string,ICoursePayment[]>,Record<string,IStudentPayments>>(cp=>{
+      const am = cp.reduce((p,ccp)=>p + ccp.amount,0);
+      return { payments:cp , amount:am}
+    },rr);
   }
 
   private lookupFirmKey(accountId: string): string | null {
